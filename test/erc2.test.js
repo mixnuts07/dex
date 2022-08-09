d0fe233c27f77517cd43bb91da0bcdde4841cbf2
mixed_nuts_nuts <french0719@gmail.com>
2022-08-09 21:27:10 +0700

    test half

--- test/erc2.test.js
+++ test/erc2.test.js
@@ -13,13 +13,16 @@ const { expect } = chai;
 chai.use(require("chai-bn")(BN));
 
 const truffleAssert = require("truffle-assertions");
+
+const toWei = (number) => web3.utils.toWei(web3.utils.toBN(number), "ether");
+
 // テスト書いていく！
 contract("ERC20 Token Test", (accounts) => {
   let dai, link, comp;
 
   const owner = accounts[0];
-  const John = accounts[1];
-  const Hassle = accounts[2];
+  const Kazu = accounts[1];
+  const Take = accounts[2];
 
   // テスト開始前に始める作業
   // contractではasyncが書けない
@@ -32,6 +35,33 @@ contract("ERC20 Token Test", (accounts) => {
   // テスト記載！
   it("should return token name and symbols correctly", async () => {
     // dai.nameを読んだ後Daiであることを期待する
+    // equal .. 同じ
+    // gte .. grate equal
+    // lte .. less than equal
+    // gt, lt など！
     expect(await dai.name()).to.equal("Dai");
+    expect(await link.symbol()).to.equal("LINK");
+  });
+
+  it("Should have Correct Total Supply", async () => {
+    expect(await comp.totalSupply()).to.bignumber.equal(toWei(10 ** 4));
+  });
+  // このテストは失敗を期待する
+  // Balanceより１多いから
+  // chaiは失敗を期待するメソッドがない
+  it("Should revert When Transfer Amount > Balance", async () => {
+    const ownerBalance = await comp.balanceOf(owner);
+    // BigNumberの足し算はこれ
+    const transferAmount = ownerBalance.add(new BN(1));
+    // reverts .. 失敗を期待
+    // passes .. 成功を期待
+    await truffleAssert.reverts(comp.transfer(Kazu, transferAmount));
+  });
+
+  // itのテストがいっぱいあると大変　→ describe
+  describe("Supply", () => {
+    // before や　after なども記述できる
+    // it を記述していく
+    // テストの分類が可能！
   });
 });
