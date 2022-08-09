3f0eced9180d6ed3aa175c9489ceedff676a2a32
mixed_nuts_nuts <french0719@gmail.com>
2022-08-10 00:40:08 +0700

    ui start

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="content">
+      <header>
+        <h3 className="header-title">TOKEN SWAP</h3>
+        <button className="connect-wallet">Connect Wallet</button>
       </header>
+      <div className="form-input">
+        <div>
+          <input type="text" />
+          <button>ETH</button>
+        </div>
+        <button>↓</button>
+        <div>
+          <input type="text" />
+          <button>SELECT TOKEN</button>
+        </div>
+        <div className="rate">
+          <h6>Exchange Rate</h6>
+          <h5>1 ETH = 3000DAI</h5>
+        </div>
+        <button>Connect Wallet</button>
+      </div>
+      <h5 className="wallet">0x0000</h5>
     </div>
   );
 }
