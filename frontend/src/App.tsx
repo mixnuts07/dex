9607b3ae1ca5af495614c86ea8a63fefca9e436a
mixed_nuts_nuts <french0719@gmail.com>
2022-08-10 18:38:55 +0700

    modal

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,50 @@
 import React from "react";
 import "./App.css";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+const NestedModal = (buttonName) => {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <Button onClick={handleOpen}>{buttonName}</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="parent-modal-title"
+        aria-describedby="parent-modal-description"
+      >
+        <Box sx={{ ...style, width: 400 }}>
+          <Button>DAI</Button>
+          <Button>LINK</Button>
+          <Button>COMP</Button>
+        </Box>
+      </Modal>
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -11,16 +56,12 @@ function App() {
       <div className="form-input">
         <div className="input-first">
           <input type="text" />
-          <button>ETH</button>
+          {/* <NestedModal buttonName="ETH" /> */}
         </div>
         <button>↓</button>
         <div className="input-to">
           <input type="text" />
-          <button>
-            SELECT
-            <br />
-            TOKEN
-          </button>
+          {/* <NestedModal buttonName="SELECT TOKEN" /> */}
         </div>
         <div className="rate">
           <h6>Exchange Rate :</h6>
