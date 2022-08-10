3a8f5c71b0487d029bc204bbc5aa8dcebb7fe17d
mixed_nuts_nuts <french0719@gmail.com>
2022-08-10 23:19:56 +0700

    一旦UI終了

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,42 +20,6 @@ const currencies = [
     label: "COMP",
   },
 ];
-const SelectTextFields = () => {
-  const [currency, setCurrency] = React.useState("");
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrency(event.target.value);
-  };
-
-  return (
-    <Box
-      component="form"
-      sx={{
-        "& .MuiTextField-root": { m: 1, width: "5em" },
-      }}
-      noValidate
-      autoComplete="off"
-    >
-      <div>
-        <TextField
-          id="outlined-select-currency"
-          select
-          label="Select"
-          value={currency}
-          onChange={handleChange}
-          helperText="Please select your currency"
-        >
-          {currencies.map((option) => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
-          ))}
-        </TextField>
-      </div>
-    </Box>
-  );
-};
-
 const CssTextField = styled(TextField)({
   "& label.Mui-focused": {
     color: "green",
@@ -76,6 +40,53 @@ const CssTextField = styled(TextField)({
   },
 });
 
+const Header = () => {
+  return (
+    <Box component="header">
+      <Box component="h3" className="header-title">
+        TOKEN SWAP
+      </Box>
+      <Button className="connect-wallet">Connect Wallet</Button>
+    </Box>
+  );
+};
+
+const SelectTextFields = () => {
+  const [currency, setCurrency] = React.useState("");
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCurrency(event.target.value);
+  };
+
+  return (
+    <Box
+      component="form"
+      sx={{
+        "& .MuiTextField-root": { m: 1, width: "5em" },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <CssTextField
+          id="outlined-select-currency"
+          select
+          label="Select"
+          value={currency}
+          onChange={handleChange}
+          helperText="Please select your currency"
+        >
+          {currencies.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </CssTextField>
+      </div>
+    </Box>
+  );
+};
+
 const CustomizedInputs = () => {
   return (
     <Box
@@ -85,23 +96,48 @@ const CustomizedInputs = () => {
         display: "flex",
         justifyContent: "center",
         flexDirection: "column",
+        alignItems: "center",
         gridTemplateColumns: { sm: "1fr 1fr" },
         gap: 2,
         width: "30vw",
         height: "40vh",
-        backgroundColor: "red",
-        margin: "auto",
+        backgroundColor: "rgba(51, 29, 29, 0.1)",
+        margin: "3em auto",
         padding: "2em",
         borderRadius: "2em",
       }}
     >
-      <Box component="div" sx={{ display: "flex", flexDirection: "row" }}>
+      <Box
+        component="div"
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          paddingTop: "8em",
+        }}
+      >
         <CssTextField label="ETH" id="custom-css-outlined-input" />
         <SelectTextFields />
       </Box>
+      <Button>↓</Button>
       <Box component="div" sx={{ display: "flex", flexDirection: "row" }}>
         <CssTextField label="SELECT TOKEN" id="custom-css-outlined-input" />
         <SelectTextFields />
+      </Box>
+      <Box component="div" className="rate">
+        <Box component="h6">Exchange Rate :</Box>
+        <Box component="h5">1 ETH = 3000DAI</Box>
+      </Box>
+      <Button>Connect Wallet</Button>
+    </Box>
+  );
+};
+
+const Footer = () => {
+  return (
+    <Box component="footer" sx={{ marginTop: "3em" }}>
+      <Box component="h5" className="wallet">
+        LOGIN ADDRESS : 0x0000
       </Box>
     </Box>
   );
@@ -110,36 +146,9 @@ const CustomizedInputs = () => {
 const App = () => {
   return (
     <Box component="div" className="content">
-      <Box component="header">
-        <Box component="h3" className="header-title">
-          TOKEN SWAP
-        </Box>
-        <Button className="connect-wallet">Connect Wallet</Button>
-      </Box>
-      {/* <Box component="div" className="form-input">
-        <Box component="div" className="input-first">
-          <input type="text" />
-          <SelectTextFields />
-        </Box>
-        <Button>↓</Button>
-        <Box component="div" className="input-to">
-          <input type="text" />
-          <SelectTextFields />
-        </Box>
-        <Box component="div" className="rate">
-          <Box component="h6">Exchange Rate :</Box>
-          <Box component="h5">1 ETH = 3000DAI</Box>
-        </Box>
-        <Button>Connect Wallet</Button>
-      </Box> */}
+      <Header />
       <CustomizedInputs />
-      {/* <SelectTextFields /> */}
-
-      <Box component="footer">
-        <Box component="h5" className="wallet">
-          0x0000
-        </Box>
-      </Box>
+      <Footer />
     </Box>
   );
 };
