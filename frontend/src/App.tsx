8139b36b6e0f26ea783d40a22aeedebe89cdb44d
mixed_nuts_nuts <french0719@gmail.com>
2022-08-10 16:19:37 +0700

    ui setting up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,22 +9,28 @@ function App() {
         <button className="connect-wallet">Connect Wallet</button>
       </header>
       <div className="form-input">
-        <div>
+        <div className="input-first">
           <input type="text" />
           <button>ETH</button>
         </div>
         <button>↓</button>
-        <div>
+        <div className="input-to">
           <input type="text" />
-          <button>SELECT TOKEN</button>
+          <button>
+            SELECT
+            <br />
+            TOKEN
+          </button>
         </div>
         <div className="rate">
-          <h6>Exchange Rate</h6>
+          <h6>Exchange Rate :</h6>
           <h5>1 ETH = 3000DAI</h5>
         </div>
         <button>Connect Wallet</button>
       </div>
-      <h5 className="wallet">0x0000</h5>
+      <footer>
+        <h5 className="wallet">0x0000</h5>
+      </footer>
     </div>
   );
 }
